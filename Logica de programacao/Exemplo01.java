3c98ae26d2423e43bfa9831a168d3154b0610e41
Franciane Lark <franciane.lark019@gmail.com>
2022-02-23 10:57:27 -0400

    update soma retangulos

---
+++ Logica de programacao/Exemplo01.java
@@ -0,0 +1,11 @@
+package mdl;
+
+public class Exemplo01 {
+    public static void main (String[]args){
+
+        int retangulo_A = 5;
+        int retangulo_B = 5;
+
+        System.out.println("Resultado =" +(retangulo_A + retangulo_B));
+    }
+}
