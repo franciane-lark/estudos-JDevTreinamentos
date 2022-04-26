fecfd181f554b70ff87335ffcfc7a7ede6c0a30e
Franciane Lark <franciane.lark019@gmail.com>
2022-04-26 12:00:28 -0400

    update - operadores relacionais

---
+++ Logica de programacao/Exemplo03.java
@@ -0,0 +1,12 @@
+public class Exemplo03 {
+    public static void main (String[] args){
+        int numero1 = 10;
+        int numero2 = 20;
+
+        if(numero1 == numero2){/*Se forem iguais ENTÃO RETORNA TRUE*/
+            System.out.println("Ebba os numero são iguais");
+        }else{
+            System.out.println("Que pena os numeros NÃO são iguais!!");
+        }
+    }
+}
