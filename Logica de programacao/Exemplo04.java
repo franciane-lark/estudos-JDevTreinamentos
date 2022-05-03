e5998f81dba94f9bd07867f46b2d8404b1296b94
Franciane Lark <franciane.lark019@gmail.com>
2022-05-02 21:37:55 -0400

    update boolean

---
+++ Logica de programacao/Exemplo04.java
@@ -0,0 +1,11 @@
+public class Exemplo04 {
+    public static void main(String[]args){
+        boolean numero1 = false;
+        boolean numero2 = false;
+        boolean numero3 = true;
+
+        if(numero1 && numero2 || numero3){
+            System.out.println("entrou na condição");
+        }
+    }
+}
